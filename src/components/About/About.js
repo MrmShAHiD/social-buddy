75348ff78ecd07e64881088b1e949eb0bb9fe6ea
SHAHID <43965492+MrmShAHiD@users.noreply.github.com>
2020-09-06 14:52:56 +0600

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+    return (
+        <div className="about-css">
+            <h5>Developed By</h5>
+            <h2>Md. Shahid-ul Islam</h2>
+            <p>This is a simple post and comments website using REACT.</p>
+        </div>
+    );
+};
+
+export default About;
